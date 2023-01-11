3db3ae0f38eb0f9a8430a2c21d567a636d732808
Pedro Henrique Santos Fagundes <64497449+porqueph@users.noreply.github.com>
2023-01-10 23:32:34 -0300

    instancias

---
+++ c_django/POO/aula1.py
@@ -0,0 +1,20 @@
+string = 'Pedro'
+
+print(string.upper())
+
+
+class Pessoa:
+    ...
+
+p1 = Pessoa()
+p1.nome = 'Pedro'
+p1.sobrenome = 'Fagundes'
+print(p1)
+print(p1.nome)
+print(p1.sobrenome)
+p2 = Pessoa()
+p2.nome = 'Query'
+p2.sobrenome = 'Appuk'
+print(p2)
+print(p2.nome)
+print(p2.sobrenome)
