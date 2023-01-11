8d7bba905912bf287b3a7152361ecb10458a7ba3
Pedro Henrique Santos Fagundes <64497449+porqueph@users.noreply.github.com>
2023-01-10 23:33:52 -0300

    instancias

---
+++ c_django/POO/intancias.py
@@ -0,0 +1,15 @@
+
+class Carro:
+    def __init__(self, nome='Sei lá'):
+        self.nome = nome
+
+    def acelerar(self):
+        print(f'{self.nome} está acelerando...')
+
+fusca = Carro('Fusca')
+print(fusca.nome)
+fusca.acelerar()
+
+celta = Carro(nome='Celta')
+print(celta.nome)
+celta.acelerar()
--- c_django/POO/introInit.py
+++ c_django/POO/introInit.py
@@ -4,20 +4,20 @@ print(string.upper())
 
 
 class Pessoa:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    def __init__(self, nome, sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
 
-p1 = Pessoa()
-p1.nome = 'Pedro'
-p1.sobrenome = 'Fagundes'
+p1 = Pessoa('Pedro', 'Fagundes')
+#p1.nome = 'Pedro'
+#p1.sobrenome = 'Fagundes'
 print(p1)
 print(p1.nome)
 print(p1.sobrenome)
 
-p2 = Pessoa()
-p2.nome = 'Query'
-p2.sobrenome = 'Appuk'
+p2 = Pessoa('Query','Apuk')
+#p2.nome = 'Query'
+#p2.sobrenome = 'Appuk'
 
 print(p2.nome)
 print(p2.sobrenome)
