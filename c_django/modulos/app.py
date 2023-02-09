566a1407bd946213184240d035a02b2b078107ac
Pedro Henrique Santos Fagundes <64497449+porqueph@users.noreply.github.com>
2023-02-09 20:04:42 -0300

    date hyper easy

---
+++ c_django/modulos/app.py
@@ -0,0 +1,3 @@
+from modulo import soma
+
+print(soma(10, 33))
---
+++ c_django/modulos/modulo.py
@@ -0,0 +1,13 @@
+
+def soma(x: float, y: float) -> float:
+    print(__name__)
+    return x + y
+    
+
+def main() -> None:
+    print(soma(10,20))
+    print(soma(22,20))
+if __name__ == "__main__":
+    print(__name__)
+    main()
+    
