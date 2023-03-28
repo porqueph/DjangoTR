0356e9e750f804b8f034428320489dc8985a6b18
Pedro Henrique Santos Fagundes <64497449+porqueph@users.noreply.github.com>
2023-03-27 23:43:45 -0300

    inicio django

--- projeto/settings.py
+++ projeto/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
+    'produto.apps.ProdutoConfig',
+    'sobre.apps.SobreConfig',
 ]
 
 MIDDLEWARE = [
